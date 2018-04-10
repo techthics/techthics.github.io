2b52496e245c6b737d1d68f46723707340c3de36
Adriana Castaneda <adriana.lcs316@gmail.com>
2018-04-10 10:28:22 -0500

    finished hamburger and slide menu

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
-var menu = document.getElementById("ham-menu");
-var bars = document.getElementById("bars");
-var body = document.getElementById("body");
-var slideMenu = document.getElementById("mainMenu");
-// var slideMenu = document.getElementsByTagName("nav");
-var clicked = true;
+// Add .is-clicked when clicked.
+$('#ham-menu').click(function() {
+  $('.line-1, .line-2, .line-3').toggleClass('is-clicked');
+  $('#site-canvas').toggleClass('move-left show');
+  $('#site-menu-right').toggleClass('hide-menu show-menu');
+});
 
-// Click Effect
-menu.addEventListener("click", activate);
-function activate(){
-  if (clicked){
-    console.log("click")
-    bars.classList.add("animation");
-    slideMenu.classList.add("menuSlide");
-    slideMenu.classList.remove("reverseMenuSlide");
-    clicked = false;
-  } else {
-    bars.classList.remove("animation");
-    slideMenu.classList.remove("menuSlide");
-    slideMenu.classList.add("reverseMenuSlide");
-    clicked = true;
-  }
-}
 
-console.log("loaded")
 
 $('.parallax').parallax({
       imageSrc: 'http://www.allwhitebackground.com/images/2/2278-190x190.jpg'
